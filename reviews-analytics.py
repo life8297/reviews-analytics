08fe1a8e3f744873418236df1d5979bd2800d55e
JR <allen8297@gmail.com>
2020-07-11 16:50:04 +0800

    calculate avg len of reviews

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -4,9 +4,10 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
-print(len(data)) # 結果為100萬筆留言
-print(data[0])
-print('--------------------')
-print(data[1])
+print('檔案讀取完了,共', len(data), '筆資料') # 結果為100萬筆留言
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('總平均留言長度為:', int(sum_len / len(data)))
