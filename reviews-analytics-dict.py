ba8fdd0bd6886af7eb7d973b63ab926d4b7592ae
JR <allen8297@gmail.com>
2020-07-19 11:34:38 +0800

    add time_count

--- reviews-analytics-dict.py
+++ reviews-analytics-dict.py
@@ -1,4 +1,5 @@
 # 分析留言數量
+import time
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
@@ -7,6 +8,7 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完了,共', len(data), '筆資料') # 結果為100萬筆留言
 print(data[0])
 
+start = time.time()
 wc = {} # word_count
 for d in data:
 	words = d.split()
@@ -15,12 +17,13 @@ for d in data:
 			wc[word] += 1 # 已有字典內容計數+ 1
 		else :
 			wc[word] = 1 # 新增新的key進字典
-# print(wc)
 
 for word in wc:
 	if wc[word] > 1000000:
 		print(word, wc[word])
+end = time.time()
 print(len(wc))
+print('讀取共花費:', int(end - start), '秒')
 
 while True:
 	search = input('請輸入查詢內容(輸入q離開):')
